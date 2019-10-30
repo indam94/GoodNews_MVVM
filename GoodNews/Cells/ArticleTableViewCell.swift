a21689a2946f9cc2ce1782afc5a0023d5c5c5d8a
DONGGUN LEE <d0n99un17@gmail.com>
2019-10-30 11:48:34 -0700

    Creating Custom Self Sizing Cells

---
+++ GoodNews/Cells/ArticleTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ArticleTableViewCell.swift
+//  GoodNews
+//
+//  Created by DONGGUN LEE on 10/30/19.
+//  Copyright © 2019 DONGGUN LEE. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ArticleTableViewCell:UITableViewCell{
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+}
--- GoodNews/Controllers/NewsListTableViewController.swift
+++ GoodNews/Controllers/NewsListTableViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 
 class NewsListTableViewController: UITableViewController{
     
+    private var articleListVM: ArticleListViewModel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -24,9 +26,36 @@ class NewsListTableViewController: UITableViewController{
         
         let url = URL(string: "https://newsapi.org/v2/top-headlines?country=us&apiKey=\(NEWS_API_KEY)")!
         
-        WebService().getArticles(url: url){_ in
+        WebService().getArticles(url: url){ articles in
             
+            if let articles = articles{
+                self.articleListVM = ArticleListViewModel(article: articles)
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
         }
     }
     
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return self.articleListVM == nil ? 0 : self.articleListVM.numberOfSection
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.articleListVM.numberOfRowInSection(section)
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "articleTableViewCell", for: indexPath) as? ArticleTableViewCell else{
+            fatalError("Not Found Cell")
+        }
+        
+        let articleVM = self.articleListVM.articleAtIndex(indexPath.row)
+        cell.titleLabel.text = articleVM.title
+        cell.descriptionLabel.text = articleVM.desciption
+        
+        return cell
+    }
 }
