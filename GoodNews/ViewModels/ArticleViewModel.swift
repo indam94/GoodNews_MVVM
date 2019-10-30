f934760b030b1e30700deafdb8f581d432eead98
DONGGUN LEE <d0n99un17@gmail.com>
2019-10-30 11:28:33 -0700

    Implementing View Models

---
+++ GoodNews/ViewModels/ArticleViewModel.swift
@@ -0,0 +1,48 @@
+//
+//  ArticleViewModel.swift
+//  GoodNews
+//
+//  Created by DONGGUN LEE on 10/30/19.
+//  Copyright © 2019 DONGGUN LEE. All rights reserved.
+//
+
+import Foundation
+
+struct ArticleListViewModel{
+    let article: [Article]
+}
+
+extension ArticleListViewModel{
+    var numberOfSection: Int{
+        return 1
+    }
+    
+    func numberOfRowInSection(_ section: Int) -> Int{
+        return self.article.count
+    }
+    
+    func articleAtIndex(_ index: Int) -> ArticleViewModel{
+        let article = self.article[index]
+        return ArticleViewModel(article)
+    }
+}
+
+struct ArticleViewModel{
+    private let article: Article
+}
+
+extension ArticleViewModel{
+    init(_ article: Article) {
+        self.article = article
+    }
+}
+
+extension ArticleViewModel{
+    var title: String{
+        return self.article.title
+    }
+    
+    var desciption: String{
+        return self.article.description
+    }
+}
